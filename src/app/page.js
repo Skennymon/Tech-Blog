05330c10ea6b6510f2ac7730aa518f6192ac4a97
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-03-06 00:19:44 -0800

    First Commit Test

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
 return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Link href="/posts">Hello</Link>
+      <h1 className="txt-9xl font-bold">Hello People! Wait it doesn't automatically refresh? Oh wait nvm it does!</h1>
     </div>
   );
 }
