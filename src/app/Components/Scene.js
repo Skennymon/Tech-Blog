1b6263afb2b320e149931be0111034c07a553747
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-03-09 00:30:05 -0800

    Made terminal funny hehe

--- src/app/Components/Scene.js
+++ src/app/Components/Scene.js
@@ -4,12 +4,12 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls, Resize } from '@react-three/drei'
 
 export default function Scene() {
-    const gltf = useLoader(GLTFLoader, '/computer_terminal.glb')
+    const gltf = useLoader(GLTFLoader, '/Kenny_Terminal_Real.glb')
     
     return (
         <div className="mt-5 h-96 flex">
             <Canvas>
-                <directionalLight intensity={15} position={[0,4,1]}/>
+                <directionalLight intensity={8} position={[0,4,1]}/>
                 <Resize scale={10}>
                     <primitive object={gltf.scene} position={[0, -1, 0]}/>
                 </Resize>
